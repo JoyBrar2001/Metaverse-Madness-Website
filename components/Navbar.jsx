cadd445f4c208138eebbbd0e9776384c9261fe6b
Joy Brar <joybrar2001@gmail.com>
2023-06-02 20:09:48 +0530

    Made some changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { navVariants } from './utils/motion';
+import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
@@ -14,17 +14,9 @@ const Navbar = () => (
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <img 
-        src="/search.svg" 
-        alt="search" 
-        className="w-[24px] h-[24px] object-contain"
-      />
+      <img src="/search.svg" alt="search" className="w-[24px] h-[24px] object-contain" />
       <h2 className="font-extrabold uppercase text-[24px] leading-[30px] text-white">Metaversus</h2>
-      <img 
-        src="menu.svg" 
-        alt="menu" 
-        className="w-[24px] h-[24px] object-contain"  
-      />
+      <img src="menu.svg" alt="menu" className="w-[24px] h-[24px] object-contain" />
     </div>
   </motion.nav>
 );
